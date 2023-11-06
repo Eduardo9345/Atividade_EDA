f3f438331bd386b416d10abb50801b0105ce7ab9
Jadson Ferreira <jadson.anderson.ferreira@gmail.com>
2023-11-06 11:19:46 -0300

    Pequenas Alterações

--- src/ListaOrdenadaComNoDescritor.java
+++ src/ListaOrdenadaComNoDescritor.java
@@ -112,6 +112,7 @@ public class ListaOrdenadaComNoDescritor implements Lista {
 	public void imprimirTamanho() {
 		System.out.println("TAMANHO: " + getTamanho());
 	}
+	
 	public void imprimirFim() {
 		System.out.println("FINAL: " + getFim().getDado());
 	}
