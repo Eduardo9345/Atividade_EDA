e4d03451aa0cb07d39a6ae4b259fb0546c754ff8
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2023-11-18 12:19:50 -0300

    Pilha estática

---
+++ src/Pilha/Estatica/Pilha.java
@@ -0,0 +1,21 @@
+package Pilha.Estatica;
+
+import Excecoes.PilhaCheiaException;
+import Excecoes.PilhaVaziaException;
+
+public interface Pilha {
+
+    public void push (int elemento) throws PilhaCheiaException;
+
+    public int pop() throws PilhaVaziaException;
+
+    public int getTopo() throws  PilhaVaziaException;
+
+    public boolean isVazia();
+
+    public boolean isCheia();
+
+    public void imprimir();
+
+    public int getQuantidade();
+}
