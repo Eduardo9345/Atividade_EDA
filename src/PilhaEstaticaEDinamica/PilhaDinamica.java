a704d69d1a5e8f03c975428119082e11031ffff7
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2023-11-18 15:18:12 -0300

    Implementar Pilha dinâmica

--- src/PilhaEstaticaEDinamica/PilhaDinamica.java
+++ src/PilhaEstaticaEDinamica/PilhaDinamica.java
@@ -4,5 +4,47 @@ import Excecoes.PilhaCheiaException;
 import Excecoes.PilhaVaziaException;
 
 public class PilhaDinamica implements Pilha{
+
+	@Override
+	public void push(int elemento) throws PilhaCheiaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int pop() throws PilhaVaziaException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTopo() throws PilhaVaziaException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isVazia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCheia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void imprimir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getQuantidade() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
